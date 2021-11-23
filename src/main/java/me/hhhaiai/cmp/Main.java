c14b6461286b579f4685fd520c6df3f0e81b4fef
sanbo.xyz <sanbo.xyz@gmail.com>
2021-11-23 11:12:24 +0800

    update

---
+++ src/main/java/me/hhhaiai/cmp/Main.java
@@ -0,0 +1,11 @@
+package me.hhhaiai.cmp;
+
+/**
+ * @Copyright © 2021 sanbo Inc. All rights reserved.
+ * @Description: TODO
+ * @Version: 1.0
+ * @Create: 2021/11/23 11:07 上午
+ * @author: sanbo
+ */
+public class Main {
+}
